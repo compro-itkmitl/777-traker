257e5a10a94766dccd906c54873d2bd8538a34ff
Pakorn233 <32859919+Pakorn233@users.noreply.github.com>
2018-04-25 18:05:25 +0700

    add comment

--- json.php
+++ json.php
@@ -5,16 +5,21 @@
 	$password = "123456";
 	$dbname = "u132998352_track";
 
+	//connect to database
 	$dbh = mysqli_connect($MyHostname, $MyUsername, $MyPassword, $My_db);
 	
+	//select the table
 	$strSQL = "SELECT * FROM tracking ";
-
+	//query database
 	$objQuery = mysqli_query($dbh, $strSQL);
 	$resultArray = array();
+
+	//process to push data to array
 	while($obResult = mysqli_fetch_assoc($objQuery))
 	{
-		array_push($resultArray,$obResult);
+		array_push($resultArray,$obResult);	
 	}
 	
+	// output the latlng array
 	echo json_encode($resultArray);
 ?>
