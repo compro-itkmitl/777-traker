e4ea2a3c0a43c9ec5dd669375988d625b4decb0b
Pakorn233 <32859919+Pakorn233@users.noreply.github.com>
2018-04-25 23:57:27 +0700

    cleaning code

--- add_data.php
+++ add_data.php
@@ -5,18 +5,17 @@ $username = "u132998352_admin";
 $password = "123456";
 $dbname = "u132998352_track";
 
-$Latitude = $_GET["t"];
-$Longtitude = $_GET["h"];
+$Lat = $_GET["t"];
+$Lng = $_GET["h"];
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
 
-$sql = "UPDATE tracking SET Latitude=$Latitude, Longtitude=$Longtitude WHERE 1";
+//update the input value from arduino to database
+$sql = "UPDATE tracking SET Latitude=$Lat, Longtitude=$Lng WHERE 1";
 $result = $conn->query($sql);
 
 $conn->close();
 ?> 
+
+<!-- url be like : ..../add_data.php?t=".."&h=".." -->
